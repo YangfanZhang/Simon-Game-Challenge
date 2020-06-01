57046f54b4946ca3871a90a1ee4cfbb69b71ca78
Yangfan Zhang <yf_chang@icloud.com>
2020-06-01 15:34:12 +1000

    Added click event handler to check which button is clicked

--- game.js
+++ game.js
@@ -1,1 +1,21 @@
-alert("working")
+
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+
+function nextSequence(){
+  var randomNumber = Math.floor(Math.random()*4);
+  var randomChosenClour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenClour);
+
+  $("#" + randomChosenClour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  var audio = new Audio("sounds/" + randomChosenClour + ".mp3");
+  audio.play();
+}
+
+$(".btn").click(function(event) {
+  var userChosenColour = event.target.id;
+  userClickedPattern.push(userChosenColour);
+  console.log(userClickedPattern);
+})
