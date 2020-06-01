65645b4f6f864cd136dc4ab918f47e5a664f9545
Yangfan Zhang <yf_chang@icloud.com>
2020-06-01 17:03:03 +1000

    Added methods to start the game.

--- game.js
+++ game.js
@@ -2,6 +2,24 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
+var startGame = false;
+var level = 0;
+
+$(document).keypress(function(){
+  if(!startGame){
+    $("h1").text("Level " + level);
+    nextSequence();
+    startGame = true;
+  }
+})
+
+$(".btn").click(function(event) {
+    var userChosenColour = event.target.id;
+    userClickedPattern.push(userChosenColour);
+    console.log(userClickedPattern);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+  })
 
 function nextSequence(){
   var randomNumber = Math.floor(Math.random()*4);
@@ -9,15 +27,9 @@ function nextSequence(){
   gamePattern.push(randomChosenClour);
   $("#" + randomChosenClour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
   playSound(randomChosenClour);
+  level++;
+  $("h1").text("Level " + level);
 }
-
-$(".btn").click(function(event) {
-  var userChosenColour = event.target.id;
-  userClickedPattern.push(userChosenColour);
-  console.log(userClickedPattern);
-  playSound(userChosenColour);
-  animatePress(userChosenColour);
-})
 
 function playSound(name) {
   var audio = new Audio("sounds/" + name + ".mp3");
