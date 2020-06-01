3fd505d52d90b66de234a8a8c8db635dc2a333e6
Yangfan Zhang <yf_chang@icloud.com>
2020-06-01 15:42:21 +1000

    Added sounds to button clicks

--- game.js
+++ game.js
@@ -7,15 +7,18 @@ function nextSequence(){
   var randomNumber = Math.floor(Math.random()*4);
   var randomChosenClour = buttonColours[randomNumber];
   gamePattern.push(randomChosenClour);
-
   $("#" + randomChosenClour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-
-  var audio = new Audio("sounds/" + randomChosenClour + ".mp3");
-  audio.play();
+  playSound(randomChosenClour);
 }
 
 $(".btn").click(function(event) {
   var userChosenColour = event.target.id;
   userClickedPattern.push(userChosenColour);
   console.log(userClickedPattern);
+  playSound(userChosenColour);
 })
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
